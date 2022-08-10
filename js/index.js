a87d971f2b4c4b46e005c3581b1738917b23c114
Rohit Dutta <91669680+rohitTo95@users.noreply.github.com>
2022-08-10 22:04:34 +0500

    Delete js directory
    Signed-off-by: Rohit Dutta <91669680+rohitTo95@users.noreply.github.com>

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const a = document.getElementById("navLinks");
-const b = document.getElementById("menuBargar")
-function menu(){
-    a.style.right ="0";
-// b.style.opacity="1";
-}
-function menuClose(){
-    a.style.right ="-700px"
-}
