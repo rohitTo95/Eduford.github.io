38253917417ee3e9f43a50e5cf6fd80580a08ea1
Rohit Dutta <91669680+rohitTo95@users.noreply.github.com>
2022-08-10 15:52:32 +0530

    This Website Is My First Responsive Web Design
    During my learning journey of web development, I make my first responsive web design.
    
      Hi, I am Rohit. A fresher in the work of software development.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const a = document.getElementById("navLinks");
+const b = document.getElementById("menuBargar")
+function menu(){
+    a.style.right ="0";
+// b.style.opacity="1";
+}
+function menuClose(){
+    a.style.right ="-700px"
+}
